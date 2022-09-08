88967b669373b4185f9cbae319cc504b28e42232
Rpetey317-WSL <rubenandrelo31@gmail.com>
2022-09-07 23:54:40 -0300

    se movio el 2do test a tests.c

--- tp1/main.c
+++ tp1/main.c
@@ -6,28 +6,13 @@
 
 int main(int argc, char *argv[]) {
 
-	//Prueba 2: abrir un csv e imprimirlo
-	caja_t* cajatest = caja_cargar_archivo("./caja1.csv");
+	printf("PRUEBA #1: crear, mostrar y destruir pokemones\n");
+	prueba1();
+	printf("FIN PRUEBA #1\n\n\n");
 
-	for (int i = 0; i < caja_cantidad(cajatest); i++){
-		int lvl, atk, def;
-		lvl = pokemon_nivel(caja_obtener_pokemon(cajatest, i));
-		atk = pokemon_ataque(caja_obtener_pokemon(cajatest, i));
-		def = pokemon_defensa(caja_obtener_pokemon(cajatest, i));
-
-		printf("---Pokemon #%i---\n", i);
-		
-		char *nombre = pokemon_nombre(caja_obtener_pokemon(cajatest, i));
-		printf("Nombre: %s\n", nombre);
-		free(nombre);
-
-		printf("Nivel: %i\n", lvl);
-		printf("Ataque: %i\n", atk);
-		printf("Defensa: %i\n", def);
-		printf("---Pokemon #%i---\n\n", i);
-	}
-
-	caja_destruir(cajatest);
+	printf("PRUEBA #2: crear, mostrar, copiar y destruir una caja\n");
+	prueba2();
+	printf("FIN PRUEBA #2\n\n\n");
 
 	return 0;
 }
