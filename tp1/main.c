299d403c1e886fa04b524980a92fb9cb1dbc206c
Rpetey317-WSL <rubenandrelo31@gmail.com>
2022-09-07 21:22:43 -0300

    creada caja_leer, caja_cant, caja_poke y caja_free

--- tp1/main.c
+++ tp1/main.c
@@ -6,7 +6,28 @@
 
 int main(int argc, char *argv[]) {
 
-	prueba1();
+	//Prueba 2: abrir un csv e imprimirlo
+	caja_t* cajatest = caja_cargar_archivo("./caja1.csv");
+
+	for (int i = 0; i < caja_cantidad(cajatest); i++){
+		int lvl, atk, def;
+		lvl = pokemon_nivel(caja_obtener_pokemon(cajatest, i));
+		atk = pokemon_ataque(caja_obtener_pokemon(cajatest, i));
+		def = pokemon_defensa(caja_obtener_pokemon(cajatest, i));
+
+		printf("---Pokemon #%i---\n", i);
+		
+		char *nombre = pokemon_nombre(caja_obtener_pokemon(cajatest, i));
+		printf("Nombre: %s\n", nombre);
+		free(nombre);
+
+		printf("Nivel: %i\n", lvl);
+		printf("Ataque: %i\n", atk);
+		printf("Defensa: %i\n", def);
+		printf("---Pokemon #%i---\n\n", i);
+	}
+
+	caja_destruir(cajatest);
 
 	return 0;
 }
--- tp1/src/cajas.c
+++ tp1/src/cajas.c
@@ -1,6 +1,9 @@
 #include "cajas.h"
 #include "pokemon.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_STRING_POKEMON 64 //str de 30 char max + 5 ints (10max c/u) + ;;;
 
 struct _caja_t {
 	pokemon_t **pokemones;
@@ -9,7 +12,35 @@ struct _caja_t {
 
 caja_t *caja_cargar_archivo(const char *nombre_archivo)
 {
-	return NULL;
+	FILE *archivo = fopen(nombre_archivo, "r");
+	if (archivo == NULL)
+		return NULL;
+
+	caja_t *caja = malloc(sizeof(caja_t));
+	if (caja == NULL)
+		return NULL;
+
+	caja->cantidad = 0;
+	caja->pokemones = malloc(sizeof(pokemon_t*));
+	if (caja->pokemones == NULL)
+		return NULL;
+
+	while (!feof(archivo)){
+		char pokemon[MAX_STRING_POKEMON];
+		fscanf(archivo, "%s\n", pokemon);
+
+		pokemon_t **temp = realloc(caja->pokemones, sizeof(pokemon_t*)*((size_t)(caja->cantidad + 1)));
+		if (temp == NULL)
+			return NULL;
+		else
+			caja->pokemones = temp;
+
+		caja->pokemones[caja->cantidad] = pokemon_crear_desde_string(pokemon);
+		caja->cantidad++;
+	}
+	
+	fclose(archivo);
+	return caja;
 }
 
 int caja_guardar_archivo(caja_t *caja, const char *nombre_archivo)
@@ -24,12 +55,21 @@ caja_t *caja_combinar(caja_t *caja1, caja_t *caja2)
 
 int caja_cantidad(caja_t *caja)
 {
-	return 0;
+	int pokes = 0;
+	if (caja != NULL)
+		pokes = caja->cantidad;
+	if (pokes < 0)
+		pokes = 0;
+	
+	return pokes;
 }
 
 pokemon_t *caja_obtener_pokemon(caja_t *caja, int n)
 {
-	return NULL;
+	if (caja == NULL || n < 0 || n >= caja->cantidad)
+		return NULL;
+	else
+		return caja->pokemones[n];
 }
 
 int caja_recorrer(caja_t *caja, void (*funcion)(pokemon_t *))
@@ -39,4 +79,10 @@ int caja_recorrer(caja_t *caja, void (*funcion)(pokemon_t *))
 
 void caja_destruir(caja_t *caja)
 {
+	if (caja != NULL){
+		for (int i = 0; i < caja->cantidad; i++)
+			free(caja->pokemones[i]);
+		free(caja->pokemones);
+		free(caja);
+	}
 }
