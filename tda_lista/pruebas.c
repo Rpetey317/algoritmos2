da0e14e344a6dbbe71d6bcd300cf4c637c85aae0
Rpetey317-WSL <rubenandrelo31@gmail.com>
2022-09-21 01:37:08 -0300

    Implementadas algunas funciones de listas

--- tda_lista/pruebas.c
+++ tda_lista/pruebas.c
@@ -3,17 +3,68 @@
 #include "src/cola.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "pa2m.h"
 
 void listaSeCreaVacia()
 {
-	//crear una lista y ver si se crea vacía?
+	lista_t *lista = lista_crear();
+	pa2m_afirmar((lista != NULL), "Puedo crear una lista");
+	pa2m_afirmar((lista_tamanio(lista) == 0), "La nueva lista está vacía");
+	pa2m_afirmar(lista_tamanio(NULL) == 0,
+		     "El tamaño de una lista nula es 0");
+	lista_destruir(lista);
+}
+
+void aniadoElementosALaLista()
+{
+	lista_t *lista = lista_crear();
+	int i0 = 0, i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5;
+	lista_insertar(lista, &i0);
+	pa2m_afirmar((lista_primero(lista) != NULL),
+		     "Puedo añadir un elemento a una lista vacía");
+	void *primero = lista_primero(lista), *ultimo = lista_ultimo(lista);
+	pa2m_afirmar(
+		(primero == ultimo),
+		"El primer y último elemento de una lista de un elemento es el mismo");
+	lista_insertar(lista, &i1);
+	lista_insertar(lista, &i2);
+	lista_insertar(lista, &i3);
+	lista_insertar(lista, &i4);
+	lista_insertar(lista, &i5);
+
+	pa2m_afirmar((lista_tamanio(lista) == 6),
+		     "El tamaño de la lista es el correcto");
+
+	pa2m_afirmar(lista_ultimo(lista) == &i5,
+		     "Los elementos nuevos se añaden al final");
+	pa2m_afirmar(lista_primero(lista) == &i0,
+		     "Añadir elementos no cambia el primer elemento");
+
+	pa2m_afirmar(
+		lista_insertar(NULL, NULL) == NULL,
+		"Intenter insertar un elemento en una lista nula devuelve nulo");
+
+	lista_insertar(lista, NULL);
+	pa2m_afirmar((lista_tamanio(lista) == 7),
+		     "Se puede añadir un puntero nulo a la lista");
+
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista, 2) == &i2,
+		"Puedo obtener el elemento en la posición 2 y es correcto");
+
+	lista_destruir(lista);
 }
 
 int main()
 {
+	printf("Pruebas locales: TDA Lista\n\n");
+
 	pa2m_nuevo_grupo("Pruebas de creación de lista");
 	listaSeCreaVacia();
+
+	pa2m_nuevo_grupo("Pruebas de añadir elemento");
+	aniadoElementosALaLista();
 
 	return pa2m_mostrar_reporte();
 }
--- tda_lista/src/lista.c
+++ tda_lista/src/lista.c
@@ -4,19 +4,73 @@
 
 lista_t *lista_crear()
 {
-	return NULL;
+	lista_t *lista = malloc(sizeof(lista_t));
+	if (!lista)
+		return NULL;
+
+	lista->nodo_inicio = NULL;
+	lista->nodo_fin = NULL;
+	lista->cantidad = 0;
+
+	return lista;
 }
 
 lista_t *lista_insertar(lista_t *lista, void *elemento)
 {
+	if (!lista)
+		return NULL;
 
-	return NULL;
+	nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
+	if (!nuevo_nodo)
+		return NULL;
+	nuevo_nodo->elemento = elemento;
+	nuevo_nodo->siguiente = NULL;
+
+	if (!(lista->nodo_inicio))
+		lista->nodo_inicio = nuevo_nodo;
+	if (lista->nodo_fin)
+		(*(lista->nodo_fin)).siguiente = nuevo_nodo;
+
+	lista->nodo_fin = nuevo_nodo;
+	lista->cantidad++;
+
+	return lista;
 }
 
 lista_t *lista_insertar_en_posicion(lista_t *lista, void *elemento,
 				    size_t posicion)
 {
-	return NULL;
+	if (!lista)
+		return NULL;
+
+	if (posicion > lista->cantidad)
+		posicion = lista->cantidad;
+
+	if (posicion == lista->cantidad) {
+		lista = lista_insertar(lista, elemento);
+	} else {
+		nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
+		if (!nuevo_nodo)
+			return NULL;
+		nuevo_nodo->elemento = elemento;
+
+		if (posicion == 0) {
+			nuevo_nodo->siguiente = lista->nodo_inicio;
+			lista->nodo_inicio = nuevo_nodo;
+		} else {
+			nodo_t *nodo_anterior = lista->nodo_inicio;
+			size_t pos_actual = 0;
+			while (pos_actual < posicion - 1 && nodo_anterior) {
+				nodo_anterior = nodo_anterior->siguiente;
+			}
+
+			nuevo_nodo->siguiente = nodo_anterior->siguiente;
+			nodo_anterior->siguiente = nuevo_nodo;
+		}
+		lista->cantidad++;
+	}
+
+	return lista;
 }
 
 void *lista_quitar(lista_t *lista)
@@ -31,7 +85,16 @@ void *lista_quitar_de_posicion(lista_t *lista, size_t posicion)
 
 void *lista_elemento_en_posicion(lista_t *lista, size_t posicion)
 {
-	return NULL;
+	if (!lista || posicion > lista->cantidad)
+		return NULL;
+
+	nodo_t *nodo_actual = lista->nodo_inicio;
+	int i = 0;
+	while (i < posicion && nodo_actual->siguiente) {
+		nodo_actual = nodo_actual->siguiente;
+		i++;
+	}
+	return nodo_actual->elemento;
 }
 
 void *lista_buscar_elemento(lista_t *lista, int (*comparador)(void *, void *),
@@ -42,27 +105,53 @@ void *lista_buscar_elemento(lista_t *lista, int (*comparador)(void *, void *),
 
 void *lista_primero(lista_t *lista)
 {
-	return NULL;
+	if (!lista)
+		return NULL;
+	return (*lista->nodo_inicio).elemento;
 }
 
 void *lista_ultimo(lista_t *lista)
 {
-	return NULL;
+	if (!lista)
+		return NULL;
+	return (*lista->nodo_fin).elemento;
 }
 
 bool lista_vacia(lista_t *lista)
 {
+	if (!lista || lista->cantidad == 0)
+		return true;
 	return false;
 }
 
 size_t lista_tamanio(lista_t *lista)
 {
-	return 0;
+	size_t tamanio = 0;
+	if (lista)
+		tamanio = lista->cantidad;
+	return tamanio;
 }
 
 void lista_destruir(lista_t *lista)
 {
+	if (!lista)
+		return;
 
+	nodo_t *nodo_actual = lista->nodo_inicio;
+	if (!nodo_actual) {
+		free(lista);
+		return;
+	}
+
+	nodo_t *nodo_siguiente = nodo_actual->siguiente;
+	while (nodo_siguiente) {
+		free(nodo_actual);
+		nodo_actual = nodo_siguiente;
+		nodo_siguiente = nodo_actual->siguiente;
+	}
+	free(nodo_actual);
+	free(lista);
+	return;
 }
 
 void lista_destruir_todo(lista_t *lista, void (*funcion)(void *))
