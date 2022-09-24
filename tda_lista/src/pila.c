1a60f8ce1c7ffd3d55ed3a4554efef6c069e7ff9
Rpetey317-WSL <rubenandrelo31@gmail.com>
2022-09-23 23:32:32 -0300

    ez

--- tda_lista/src/pila.c
+++ tda_lista/src/pila.c
@@ -1,37 +1,39 @@
 #include "pila.h"
 #include "lista.h"
 
+typedef struct lista_t _pila_t;
+
 pila_t *pila_crear()
 {
-	return NULL;
+	return (pila_t *)lista_crear();
 }
 
 pila_t *pila_apilar(pila_t *pila, void *elemento)
 {
-	return NULL;
+	return (pila_t *)lista_insertar((lista_t *)pila, elemento);
 }
 
 void *pila_desapilar(pila_t *pila)
 {
-	return NULL;
+	return lista_quitar((lista_t *)pila);
 }
 
 void *pila_tope(pila_t *pila)
 {
-	return NULL;
+	return lista_ultimo((lista_t *)pila);
 }
 
 size_t pila_tamanio(pila_t *pila)
 {
-	return 0;
+	return lista_tamanio((lista_t *)pila);
 }
 
 bool pila_vacia(pila_t *pila)
 {
-	return false;
+	return lista_vacia((lista_t *)pila);
 }
 
 void pila_destruir(pila_t *pila)
 {
-
+	lista_destruir((lista_t *)pila);
 }
