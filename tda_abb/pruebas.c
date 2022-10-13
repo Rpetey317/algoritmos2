17fe765899d4f488a92c6d960a8df4b97b96ef19
Rpetey317-WSL <rubenandrelo31@gmail.com>
2022-10-13 18:29:29 -0300

    Añadido desde la página de la materia

---
+++ tda_abb/pruebas.c
@@ -0,0 +1,17 @@
+#include "src/abb.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pa2m.h"
+
+void abbSeCreaVacio()
+{
+	//crear un abb y ver si se crea vacío?
+}
+
+int main()
+{
+	pa2m_nuevo_grupo("Pruebas de creación de abb");
+	abbSeCreaVacio();
+
+	return pa2m_mostrar_reporte();
+}
